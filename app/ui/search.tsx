2328293c71598e16c414f5a8524e24b4f696381a
Willian Custodio Quintino <suporte5.dev@grupoepj.com.br>
2024-07-17 17:35:05 -0300

    11. Adicionando Pesquisa e Paginação: 4. Atualizando a tabela: 4. Atualizando a tabela

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -17,7 +17,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
   }
-  
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -29,6 +29,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
